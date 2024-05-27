803bc70f8c142f4cf2b40b1b22b92fcf924486a6
Henrique Eidt <henriqueidt@gmail.com>
2024-05-26 23:25:22 -0300

    Create tokenizer for simple characters

--- src/fileParser.ts
+++ src/fileParser.ts
@@ -1,9 +1,5 @@
 import { tokenize } from "./tokenizer";
 
 export const parseFile = (data: string) => {
-  // const lines = data.split("\n");
   return tokenize(data);
-  // return lines.map((line) => {
-  //   return tokenize(line);
-  // });
 };
